ab8db45a2917601476f8304f1d5e19fb58218f87
Alex Olmedo <aricardoolopes@gmail.com>
2024-07-31 14:12:38 -0300

    Contando jogos alugados

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector(".dashboard__item__img");
     let botao = gameClicado.querySelector(".dashboard__item__button");
+    let nomeJogo = gameClicado.querySelector(".dashboard__item__name").textContent;
 
     if (imagem.classList.contains("dashboard__item__img--rented")) {
+        if (confirm(`Você deseja devolver ${nomeJogo}?`) === false) {
+            return;
+        }
         imagem.classList.remove("dashboard__item__img--rented");
         botao.classList.remove("dashboard__item__button--return");
         botao.textContent = "Alugar";
@@ -12,4 +16,11 @@ function alterarStatus(id) {
         botao.textContent = "Devolver";
         botao.classList.add("dashboard__item__button--return");
     }
+
+    contarJogosAlugados();
+}
+
+function contarJogosAlugados () {
+    let jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`Você possui ${jogosAlugados} jogos alugados.`);
 }
